b7c1b23068086b61ffe32d12b3931b7456138485
Guanhua Gao <guanhua.gao@nxp.com>
2020-07-16 10:55:30 +0800

    PCI: layerscape: Set 64-bit DMA mask
    Set DMA mask and coherent DMA mask to enable 64-bit addressing.
    
    Signed-off-by: Guanhua Gao <guanhua.gao@nxp.com>
    Signed-off-by: Hou Zhiqiang <Zhiqiang.Hou@nxp.com>
    (forward ported from commit 698f39ac088a5f64e1460736e6d56e27690e980e nxp-kernel)
    Signed-off-by: Wen-chien Jesse Sung <jesse.sung@canonical.com>

--- drivers/pci/controller/dwc/pci-layerscape-ep.c
+++ drivers/pci/controller/dwc/pci-layerscape-ep.c
@@ -265,7 +265,9 @@ static int __init ls_pcie_ep_probe(struct platform_device *pdev)
 
 	pcie->big_endian = of_property_read_bool(dev->of_node, "big-endian");
 
-	dma_set_mask_and_coherent(dev, DMA_BIT_MASK(64));
+	/* set 64-bit DMA mask and coherent DMA mask */
+	if (dma_set_mask_and_coherent(dev, DMA_BIT_MASK(64)))
+		dev_warn(dev, "Failed to set 64-bit DMA mask.\n");
 
 	platform_set_drvdata(pdev, pcie);
 
