bbfc84bf6b6f6aff7c622b56bdb114597f86de56
Sachin Gupta <quic_sachgupt@quicinc.com>
2024-08-30 11:19:19 +0530

    mmc: sdhci-msm: Disable clock-gating for SDIO card
    This change will disable clk-gating for SDIO cards.
    
    Change-Id: I23d708cba73d771b034fefd8125188b941c8295b
    Signed-off-by: Sachin Gupta <quic_sachgupt@quicinc.com>

--- drivers/mmc/core/host.c
+++ drivers/mmc/core/host.c
@@ -215,8 +215,8 @@ bool mmc_host_may_gate_card(struct mmc_card *card)
 	 * SDIO3.0 card allows the clock to be gated off so check if
 	 * that is the case or not.
 	 */
-	if (mmc_card_sdio(card) && card->cccr.async_intr_sup)
-		return true;
+	if (mmc_card_sdio(card))
+		return false;
 
 	/*
 	 * Don't gate SDIO cards! These need to be clocked at all times
