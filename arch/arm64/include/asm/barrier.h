boundarydevices/linux

MLK-25546 arm64: mm: barrier Fix 8QM usb stop when work with GPU
The below commit change dma_barrier from dsb to dmb.
	commit 22ec71615d824f4f11d38d0e55a88d8956b7e45f
	Author: Will Deacon <will@kernel.org>
	arm64: io: Relax implicit barriers in default I/O accessors

in 8QM, SYSBARDISABLE is 0. That's means barrier will propagate to
cci 400.

cci 400 behavior when handle barrier.

For barriers that are system shareable, that is, AxDOMAIN is 0b11,
the barrier response is sent by the CCI-400 when all pre-barrier
transactions from the barrier issuer are complete.

For other types of barrier, the response is sent when snoop responses
are received for all earlier transactions. Transactions received after
the barrier are then address-hazard-checked against all outstanding
pre-barrier transactions.

        ┌──────┐ ┌────┐
        │ A53  │ │A72 │
        └───┬──┘ └─┬──┘
            │      │
        ┌───▼──────▼──┐
        │    CCI400   │
        └─────┬───────┘
              │   1 (a)write to ddr (normal uncached memory)
              │   barrer()
              │   2 (b)write to usb register(device, nGnRE)
        ┌─────▼───────────────────────┐       ┌───────────┐
        │                             ◄───────┤   GPU     │
        │     Bus fabric              │       │           │
        └────────────────────────────┬┘       └───────────┘
3 (b) reach usb   ▲ 4 usb read   ▲   │ 6.(a)reach
         │        │   ddr        │   │
      ┌──▼────────┴─┐            │   │
      │             │            │   │
      │  USB        │      5.usb │   │
      │             │      read  │   │
      └─────────────┘            │   │
                               ┌─┴───▼─┐
                               │       │
                               │ DDR   │
                               │       │
                               └───────┘

cci 400 will response barrier (oshst) before 1(a) write to ddr.
so 3(b) reach usb before 1(a) arrive ddr. so usb get old dma descriptor,
which dma descri

arch/arm64/include/asm/barrier.h
@@ -45,9 +45,9 @@
 #define rmb()		dsb(ld)
 #define wmb()		dsb(st)
 
-#define dma_mb()	dmb(osh)
-#define dma_rmb()	dmb(oshld)
-#define dma_wmb()	dmb(oshst)
+#define dma_mb()	dmb(sy)
+#define dma_rmb()	dmb(ld)
+#define dma_wmb()	dmb(st)
 
 /*
  * Generate a mask for array_index__nospec() that is ~0UL when 0 <= idx < sz

CodeLinaro-mirror/femto5g_kernel_msm-4.14

Signed-off-by: Ondrej Kubik <ondrej.kubik@canonical.com>

arch/arm64/include/asm/barrier.h
@@ -61,9 +61,9 @@
 #define __rmb()		dsb(ld)
 #define __wmb()		dsb(st)
 
-#define __dma_mb()	dmb(osh)
-#define __dma_rmb()	dmb(oshld)
-#define __dma_wmb()	dmb(oshst)
+#define __dma_mb()	dmb(sy)
+#define __dma_rmb()	dmb(ld)
+#define __dma_wmb()	dmb(st)
 
 #define io_stop_wc()	dgh()
 