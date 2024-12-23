ba0b0d85a68f8ab6d653e3689e37b661fadb9c70
Gao Wang <quic_gaowang@quicinc.com>
2024-12-23 19:02:57 +0800

    msm: npu: Fix use after free issue
    There is possibility that network will be used after free.
    This change is to fix this issue.
    
    Change-Id: I598abf51ddb2d2aa423b0861f62a2cd751cbb1b0
    Signed-off-by: Gao Wang <quic_gaowang@quicinc.com>
    (cherry picked from commit 3a50d9f535c758025e6d1487cec3e39430d742b2)

--- drivers/media/platform/msm/npu_v2/npu_mgr.c
+++ drivers/media/platform/msm/npu_v2/npu_mgr.c
@@ -2487,8 +2487,22 @@ int32_t npu_host_unload_network(struct npu_client *client,
 		return -EINVAL;
 	}
 
+	if (network->is_unloading) {
+		NPU_ERR("network is unloading\n");
+		network_put(network);
+		mutex_unlock(&host_ctx->lock);
+		return -EINVAL;
+	}
+
 	if (!network->is_active) {
 		NPU_ERR("network is not active\n");
+		network_put(network);
+		mutex_unlock(&host_ctx->lock);
+		return -EINVAL;
+	}
+
+	if (network->is_executing) {
+		NPU_ERR("network is in execution\n");
 		network_put(network);
 		mutex_unlock(&host_ctx->lock);
 		return -EINVAL;
@@ -2498,6 +2512,8 @@ int32_t npu_host_unload_network(struct npu_client *client,
 		NPU_ERR("fw in error state, skip unload network in fw\n");
 		goto free_network;
 	}
+
+	network->is_unloading = true;
 
 	NPU_DBG("Unload network %lld\n", network->id);
 	/* prepare IPC packet for UNLOAD */
@@ -2629,8 +2645,20 @@ int32_t npu_host_exec_network_v2(struct npu_client *client,
 		return -EINVAL;
 	}
 
+	if (network->is_unloading) {
+		NPU_ERR("network is unloading\n");
+		ret = -EINVAL;
+		goto exec_v2_done;
+	}
+
 	if (!network->is_active) {
 		NPU_ERR("network is not active\n");
+		ret = -EINVAL;
+		goto exec_v2_done;
+	}
+
+	if (network->is_executing) {
+		NPU_ERR("network is already in execution\n");
 		ret = -EINVAL;
 		goto exec_v2_done;
 	}
@@ -2652,6 +2680,7 @@ int32_t npu_host_exec_network_v2(struct npu_client *client,
 		goto exec_v2_done;
 	}
 
+	network->is_executing = true;
 	for (i = 0; i < num_patch_params; i++) {
 		exec_packet->patch_params[i].id = patch_buf_info[i].buf_id;
 		NPU_DBG("%d: patch_id: %x\n", i,
@@ -2755,6 +2784,7 @@ free_exec_cmd:
 	npu_free_network_cmd(host_ctx, exec_cmd);
 free_exec_packet:
 	kfree(exec_packet);
+	network->is_executing = false;
 exec_v2_done:
 	network_put(network);
 	mutex_unlock(&host_ctx->lock);
