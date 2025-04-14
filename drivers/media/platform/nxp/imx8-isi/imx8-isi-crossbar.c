boundarydevices/linux

LF-11913: media: imx8-isi: fix invalid frame description issue
For iMX95, the stream configurations are moved to csi formatter from
gasket enable/disable callback and only leave ISI QoS setting. When
enable multiple stream support in ISI driver, it will get multiple
frame description entity from remote subdev, but for gasket, only
need be called once, so remove the checking for the number of frame
description entity and use the first stream for single camera platform.

Signed-off-by: Guoniu.zhou <guoniu.zhou@nxp.com>
Reviewed-by: Robby Cai <robby.cai@nxp.com>
Reviewed-by: Laurentiu Palcu <laurentiu.palcu@oss.nxp.com>

drivers/media/platform/nxp/imx8-isi/imx8-isi-crossbar.c
@@ -35,6 +35,7 @@ static int mxc_isi_crossbar_gasket_enable(struct mxc_isi_crossbar *xbar,
 	const struct mxc_gasket_ops *gasket_ops = isi->pdata->gasket_ops;
 	const struct v4l2_mbus_framefmt *fmt;
 	struct v4l2_mbus_frame_desc fd;
+	unsigned int stream;
 	int ret;
 
 	if (!gasket_ops)
@@ -54,13 +55,12 @@ static int mxc_isi_crossbar_gasket_enable(struct mxc_isi_crossbar *xbar,
 		return ret;
 	}
 
-	if (fd.num_entries != 1) {
-		dev_err(isi->dev, "invalid frame descriptor for '%s':%u\n",
-			remote_sd->name, remote_pad);
-		return -EINVAL;
-	}
-
-	fmt = v4l2_subdev_state_get_stream_format(state, port, 0);
+	/*
+	 * For single or multiple stream, only the first stream be used
+	 * since gasket enable callback be called only once.
+	 */
+	stream = fd.num_entries > 0 ? fd.entry[0].stream : 0;
+	fmt = v4l2_subdev_state_get_stream_format(state, port, stream);
 	if (!fmt)
 		return -EINVAL;
 

CodeLinaro-mirror/femto5g_kernel_msm-4.14

(forward ported from commit f02a70ea4ee33f57abddf624808d720e3e62eea0 nxp-kernel)
Signed-off-by: Wen-chien Jesse Sung <jesse.sung@canonical.com>

drivers/media/platform/nxp/imx8-isi/imx8-isi-crossbar.c
@@ -35,6 +35,7 @@ static int mxc_isi_crossbar_gasket_enable(struct mxc_isi_crossbar *xbar,
 	const struct mxc_gasket_ops *gasket_ops = isi->pdata->gasket_ops;
 	const struct v4l2_mbus_framefmt *fmt;
 	struct v4l2_mbus_frame_desc fd;
+	unsigned int stream;
 	int ret;
 
 	if (!gasket_ops)
@@ -54,13 +55,12 @@ static int mxc_isi_crossbar_gasket_enable(struct mxc_isi_crossbar *xbar,
 		return ret;
 	}
 
-	if (fd.num_entries != 1) {
-		dev_err(isi->dev, "invalid frame descriptor for '%s':%u\n",
-			remote_sd->name, remote_pad);
-		return -EINVAL;
-	}
-
-	fmt = v4l2_subdev_state_get_format(state, port, 0);
+	/*
+	 * For single or multiple stream, only the first stream be used
+	 * since gasket enable callback be called only once.
+	 */
+	stream = fd.num_entries > 0 ? fd.entry[0].stream : 0;
+	fmt = v4l2_subdev_state_get_format(state, port, stream);
 	if (!fmt)
 		return -EINVAL;
 