boundarydevices/linux

LF-10571-2: ASoC: fsl-asoc-card: Flip DAI clock format
Set flipped DAI hardware clock providers/consumers format for CPU DAI
side.

Signed-off-by: Chancel Liu <chancel.liu@nxp.com>
Reviewed-by: Shengjiu Wang <shengjiu.wang@nxp.com>

sound/soc/fsl/fsl-asoc-card.c
@@ -299,7 +299,8 @@ static int fsl_asoc_card_hw_params(struct snd_pcm_substream *substream,
 						 cpu_priv->slot_width);
 		}
 		/* set cpu DAI configuration */
-		ret = snd_soc_dai_set_fmt(asoc_rtd_to_cpu(rtd, 0), priv->dai_fmt);
+		ret = snd_soc_dai_set_fmt(asoc_rtd_to_cpu(rtd, 0),
+					  snd_soc_daifmt_clock_provider_flipped(priv->dai_fmt));
 		if (ret) {
 			dev_err(dev, "failed to set cpu dai fmt: %d\n", ret);
 			return ret;

CodeLinaro-mirror/femto5g_kernel_msm-4.14

(forward ported from commit 1f86b8f21acc751efa19d2eed26ba5da7473c67c nxp-kernel)
Signed-off-by: Wen-chien Jesse Sung <jesse.sung@canonical.com>

sound/soc/fsl/fsl-asoc-card.c
@@ -299,7 +299,8 @@ static int fsl_asoc_card_hw_params(struct snd_pcm_substream *substream,
 						 cpu_priv->slot_width);
 		}
 		/* set cpu DAI configuration */
-		ret = snd_soc_dai_set_fmt(snd_soc_rtd_to_cpu(rtd, 0), priv->dai_fmt);
+		ret = snd_soc_dai_set_fmt(snd_soc_rtd_to_cpu(rtd, 0),
+					  snd_soc_daifmt_clock_provider_flipped(priv->dai_fmt));
 		if (ret) {
 			dev_err(dev, "failed to set cpu dai fmt: %d\n", ret);
 			return ret;