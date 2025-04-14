boundarydevices/linux

LF-7285: firmware: imx: ele: Add support for ELE HW Random Number Generator
- removed static from imx_ele_msg_send_rcv api.
  it is used to send and receive message over message
  unit(mu) to communicate with S40x.
  making this non static so other driver can use this
  api to interact with S40x.

- moved the inclusion of mailbox_client.h to ele_mu.h

- added ele_get_trng_state to read the TRNG state.
  ele mu driver use this to check if TRNG entropy is valid
  and ready to be read.

- added ele_start_rng to start the initialization of the Sentinel RNG.
  ele mu driver use this to start Sentinel RNG when trng state
  is not valid.

- added ele_trng_enable in  ele mu driver for initializing sentinel TRNG.

- added ele_trng_init to register Hardware Random Number Generator driver

- added ele_get_random to proceed with random number generation operation.
  ele hwrng driver use this to read the random number with Sentinel.

Signed-off-by: Gaurav Jain <gaurav.jain@nxp.com>
Reviewed-by: Pankaj Gupta <pankaj.gupta@nxp.com>
Reviewed-by: Varun Sethi <V.Sethi@nxp.com>

include/linux/firmware/imx/ele_base_msg.h
@@ -14,6 +14,7 @@
 #define MAX_MESSAGE_SIZE_BYTES (MAX_MESSAGE_SIZE * sizeof(u32))
 
 #define MESSAGING_VERSION_6		0x6
+#define MESSAGING_VERSION_7		0x7
 
 #define ELE_PING_REQ			0x1
 #define ELE_OEM_CNTN_AUTH_REQ		0x87
@@ -24,6 +25,10 @@
 #define OTFAD_CONFIG			0x2
 #define ELE_GET_INFO_REQ                0xDA
 #define GET_INFO_DATA                   0x17
+#define ELE_START_RNG_REQ		0xA3
+#define ELE_GET_TRNG_STATE_REQ		0xA4
+#define ELE_GET_RANDOM_REQ		0x22
+#define CSAL_TRNG_STATE_MASK		0x0000ffff
 
 #define ELE_VERSION			0x6
 #define ELE_SUCCESS_IND			0xD6
@@ -39,5 +44,7 @@
 int read_common_fuse(uint16_t fuse_index, u32 *value);
 int ele_ping(void);
 int ele_get_info(phys_addr_t addr, u32 data_size);
+int ele_start_rng(void);
+int ele_get_trng_state(void);
 
 #endif

CodeLinaro-mirror/femto5g_kernel_msm-4.14

Squashed:
LF-7786-1: firmware: imx: ele: split ele_trng_enable code
LF-8559 firmware: imx: ele: Update RNG request id
(cherry picked from commit bc7863a62416782bf06c20eb13c48154a2eaeffb nxp-kernel)
Signed-off-by: Wen-chien Jesse Sung <jesse.sung@canonical.com>

include/linux/firmware/imx/ele_base_msg.h
@@ -14,6 +14,7 @@
 #define MAX_MESSAGE_SIZE_BYTES (MAX_MESSAGE_SIZE * sizeof(u32))
 
 #define MESSAGING_VERSION_6		0x6
+#define MESSAGING_VERSION_7		0x7
 
 #define ELE_PING_REQ			0x1
 #define ELE_OEM_CNTN_AUTH_REQ		0x87
@@ -24,6 +25,10 @@
 #define OTFAD_CONFIG			0x2
 #define ELE_GET_INFO_REQ                0xDA
 #define GET_INFO_DATA                   0x17
+#define ELE_START_RNG_REQ		0xA3
+#define ELE_GET_TRNG_STATE_REQ		0xA4
+#define ELE_GET_RANDOM_REQ		0xCD
+#define CSAL_TRNG_STATE_MASK		0x0000ffff
 
 #define ELE_VERSION			0x6
 #define ELE_SUCCESS_IND			0xD6
@@ -39,5 +44,7 @@
 int read_common_fuse(uint16_t fuse_index, u32 *value, bool special_id);
 int ele_ping(void);
 int ele_get_info(phys_addr_t addr, u32 data_size);
+int ele_start_rng(void);
+int ele_get_trng_state(void);
 
 #endif