9ee80b7c65fe77dcc04a8850ac40be16b2891c87
Nikhil Gautam <quic_nikhgaut@quicinc.com>
2025-06-17 14:16:20 +0530

    msm: adsprpc: added CONFIG_DEBUG_FS check on debugfs related apis
    Added CONFIG_DEBUG_FS checks around debugfs variables and apis
    So that failure log should not come if we disable the DEBUG_FS
    
    Change-Id: Ibea1b20ec305e31a8e1dcdded89f01ad5aff0ff5
    Signed-off-by: Nikhil Gautam <quic_nikhgaut@quicinc.com>

--- drivers/char/adsprpc.c
+++ drivers/char/adsprpc.c
@@ -48,7 +48,11 @@
 #include "adsprpc_compat.h"
 #include "adsprpc_shared.h"
 #include <soc/qcom/ramdump.h>
+
+#ifdef CONFIG_DEBUG_FS
 #include <linux/debugfs.h>
+#endif
+
 #include <linux/pm_qos.h>
 
 #define TZ_PIL_PROTECT_MEM_SUBSYS_ID 0x0C
@@ -134,8 +138,11 @@
 static int fastrpc_pdr_notifier_cb(struct notifier_block *nb,
 					unsigned long code,
 					void *data);
+
+#ifdef CONFIG_DEBUG_FS
 static struct dentry *debugfs_root;
 static struct dentry *debugfs_global_file;
+#endif
 
 static inline uint64_t buf_page_start(uint64_t buf)
 {
@@ -3052,7 +3059,9 @@ static int fastrpc_file_free(struct fastrpc_file *fl)
 	spin_lock(&fl->apps->hlock);
 	hlist_del_init(&fl->hn);
 	spin_unlock(&fl->apps->hlock);
+#ifdef CONFIG_DEBUG_FS
 	kfree(fl->debug_buf);
+#endif
 
 	if (!fl->sctx) {
 		kfree(fl);
@@ -3111,14 +3120,17 @@ static int fastrpc_device_release(struct inode *inode, struct file *file)
 	if (fl) {
 		if (fl->qos_request && pm_qos_request_active(&fl->pm_qos_req))
 			pm_qos_remove_request(&fl->pm_qos_req);
+#ifdef CONFIG_DEBUG_FS
 		if (fl->debugfs_file != NULL)
 			debugfs_remove(fl->debugfs_file);
+#endif
 		fastrpc_file_free(fl);
 		file->private_data = NULL;
 	}
 	return 0;
 }
 
+#ifdef CONFIG_DEBUG_FS
 static int fastrpc_debugfs_open(struct inode *inode, struct file *filp)
 {
 	filp->private_data = inode->i_private;
@@ -3359,6 +3371,8 @@ static const struct file_operations debugfs_fops = {
 	.open = fastrpc_debugfs_open,
 	.read = fastrpc_debugfs_read,
 };
+#endif
+
 static int fastrpc_channel_open(struct fastrpc_file *fl)
 {
 	struct fastrpc_apps *me = &gfa;
@@ -3473,6 +3487,7 @@ static int fastrpc_set_process_info(struct fastrpc_file *fl)
 	cur_comm[TASK_COMM_LEN-1] = '\0';
 	fl->tgid = current->tgid;
 	snprintf(strpid, PID_SIZE, "%d", current->pid);
+#ifdef CONFIG_DEBUG_FS
 	if (debugfs_root) {
 		buf_size = strlen(cur_comm) + strlen("_")
 			+ strlen(strpid) + 1;
@@ -3501,6 +3516,7 @@ static int fastrpc_set_process_info(struct fastrpc_file *fl)
 			fl->debug_buf = NULL;
 		}
 	}
+#endif
 	return err;
 }
 
@@ -4110,6 +4126,7 @@ static int fastrpc_cb_probe(struct device *dev)
 	}
 
 	chan->sesscount++;
+#ifdef CONFIG_DEBUG_FS
 	if (debugfs_root && !debugfs_global_file) {
 		debugfs_global_file = debugfs_create_file("global", 0644,
 			debugfs_root, NULL, &debugfs_fops);
@@ -4119,6 +4136,7 @@ static int fastrpc_cb_probe(struct device *dev)
 			debugfs_global_file = NULL;
 		}
 	}
+#endif
 bail:
 	return err;
 }
@@ -4528,7 +4546,7 @@ static int __init fastrpc_device_init(void)
 	struct device *dev = NULL;
 	struct device *secure_dev = NULL;
 	int err = 0, i;
-
+#ifdef CONFIG_DEBUG_FS
 	debugfs_root = debugfs_create_dir("adsprpc", NULL);
 	if (IS_ERR_OR_NULL(debugfs_root)) {
 		pr_warn("Error: %s: %s: failed to create debugfs root dir\n",
@@ -4536,6 +4554,7 @@ static int __init fastrpc_device_init(void)
 		debugfs_remove_recursive(debugfs_root);
 		debugfs_root = NULL;
 	}
+#endif
 	memset(me, 0, sizeof(*me));
 	fastrpc_init(me);
 	me->dev = NULL;
@@ -4671,7 +4690,9 @@ static void __exit fastrpc_device_exit(void)
 		wakeup_source_unregister(me->wake_source);
 	if (me->wake_source_secure)
 		wakeup_source_unregister(me->wake_source_secure);
+#ifdef CONFIG_DEBUG_FS
 	debugfs_remove_recursive(debugfs_root);
+#endif
 }
 
 late_initcall(fastrpc_device_init);
