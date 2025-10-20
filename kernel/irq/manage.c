cc27be26fcb80877287198c1b6c41111dab9bb7f
Marc Zyngier <maz@kernel.org>
2025-10-20 13:29:33 +0100

    BACKPORT: UPSTREAM: genirq: Allow per-cpu interrupt sharing for non-overlapping affinities
    Interrupt sharing for percpu-devid interrupts is forbidden, and for good
    reasons. These are interrupts generated *from* a CPU and handled by itself
    (timer, for example). Nobody in their right mind would put two devices on
    the same pin (and if they have, they get to keep the pieces...).
    
    But this also prevents more benign cases, where devices are connected
    to groups of CPUs, and for which the affinities are not overlapping.
    Effectively, the only thing they share is the interrupt number, and
    nothing else.
    
    Tweak the definition of IRQF_SHARED applied to percpu_devid interrupts to
    allow this particular use case. This results in extra validation at the
    point of the interrupt being setup and freed, as well as a tiny bit of
    extra complexity for interrupts at handling time (to pick the correct
    irqaction).
    
    Signed-off-by: Marc Zyngier <maz@kernel.org>
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Tested-by: Will Deacon <will@kernel.org>
    Link: https://patch.msgid.link/20251020122944.3074811-17-maz@kernel.org
    (cherry picked from commit bdf4e2ac295fe77c94b570a1ad12c0882bc89b53)
    [willdeacon@: Fix error paths in __free_percpu_irq() to unlock spinlock
     due to lack of scoped_guard()]
    Signed-off-by: Will Deacon <willdeacon@google.com>
    Change-Id: I8004361fb951dee2fdefc165ed58add976af92cd
    Bug: 377953908

--- kernel/irq/manage.c
+++ kernel/irq/manage.c
@@ -1494,6 +1494,19 @@ setup_irq_thread(struct irqaction *new, unsigned int irq, bool secondary)
 	return 0;
 }
 
+static bool valid_percpu_irqaction(struct irqaction *old, struct irqaction *new)
+{
+	do {
+		if (cpumask_intersects(old->affinity, new->affinity) ||
+		    old->percpu_dev_id == new->percpu_dev_id)
+			return false;
+
+		old = old->next;
+	} while (old);
+
+	return true;
+}
+
 /*
  * Internal function to register an irqaction - typically used to
  * allocate special interrupts that are part of the architecture.
@@ -1514,6 +1527,7 @@ __setup_irq(unsigned int irq, struct irq_desc *desc, struct irqaction *new)
 	struct irqaction *old, **old_ptr;
 	unsigned long flags, thread_mask = 0;
 	int ret, nested, shared = 0;
+	bool per_cpu_devid;
 
 	if (!desc)
 		return -EINVAL;
@@ -1522,6 +1536,8 @@ __setup_irq(unsigned int irq, struct irq_desc *desc, struct irqaction *new)
 		return -ENOSYS;
 	if (!try_module_get(desc->owner))
 		return -ENODEV;
+
+	per_cpu_devid = irq_settings_is_per_cpu_devid(desc);
 
 	new->irq = irq;
 
@@ -1630,8 +1646,15 @@ __setup_irq(unsigned int irq, struct irq_desc *desc, struct irqaction *new)
 		 */
 		unsigned int oldtype;
 
-		if (irq_is_nmi(desc)) {
+		if (irq_is_nmi(desc) && !per_cpu_devid) {
 			pr_err("Invalid attempt to share NMI for %s (irq %d) on irqchip %s.\n",
+				new->name, irq, desc->irq_data.chip->name);
+			ret = -EINVAL;
+			goto out_unlock;
+		}
+
+		if (per_cpu_devid && !valid_percpu_irqaction(old, new)) {
+			pr_err("Overlapping affinities for %s (irq %d) on irqchip %s.\n",
 				new->name, irq, desc->irq_data.chip->name);
 			ret = -EINVAL;
 			goto out_unlock;
@@ -1787,7 +1810,7 @@ __setup_irq(unsigned int irq, struct irq_desc *desc, struct irqaction *new)
 		if (!(new->flags & IRQF_NO_AUTOEN) &&
 		    irq_settings_can_autoenable(desc)) {
 			irq_startup(desc, IRQ_RESEND, IRQ_START_COND);
-		} else {
+		} else if (!per_cpu_devid) {
 			/*
 			 * Shared interrupts do not go well with disabling
 			 * auto enable. The sharing interrupt might request
@@ -2467,7 +2490,7 @@ void disable_percpu_nmi(unsigned int irq)
 static struct irqaction *__free_percpu_irq(unsigned int irq, void __percpu *dev_id)
 {
 	struct irq_desc *desc = irq_to_desc(irq);
-	struct irqaction *action;
+	struct irqaction *action, **action_ptr;
 	unsigned long flags;
 
 	WARN(in_interrupt(), "Trying to free IRQ %d from IRQ context!\n", irq);
@@ -2477,11 +2500,33 @@ static struct irqaction *__free_percpu_irq(unsigned int irq, void __percpu *dev_
 
 	raw_spin_lock_irqsave(&desc->lock, flags);
 
-	action = desc->action;
-	if (!action || action->percpu_dev_id != dev_id) {
-		WARN(1, "Trying to free already-free IRQ %d\n", irq);
+	action_ptr = &desc->action;
+	for (;;) {
+		action = *action_ptr;
+
+		if (!action) {
+			WARN(1, "Trying to free already-free IRQ %d\n", irq);
+			goto bad;
+		}
+
+		if (action->percpu_dev_id == dev_id)
+			break;
+
+		action_ptr = &action->next;
+	}
+
+	if (cpumask_intersects(desc->percpu_enabled, action->affinity)) {
+		WARN(1, "percpu IRQ %d still enabled on CPU%d!\n", irq,
+		     cpumask_first_and(desc->percpu_enabled, action->affinity));
 		goto bad;
 	}
+
+	/* Found it - now remove it from the list of entries: */
+	*action_ptr = action->next;
+
+	/* Demote from NMI if we killed the last action */
+	if (!desc->action)
+		desc->istate &= ~IRQS_NMI;
 
 	if (!cpumask_empty(desc->percpu_enabled)) {
 		WARN(1, "percpu IRQ %d still enabled on CPU%d!\n",
@@ -2606,6 +2651,14 @@ struct irqaction *create_percpu_irqaction(irq_handler_t handler, unsigned long f
 	action->name = devname;
 	action->percpu_dev_id = dev_id;
 	action->affinity = affinity;
+
+	/*
+	 * We allow some form of sharing for non-overlapping affinity
+	 * masks. Obviously, covering all CPUs prevents any sharing in
+	 * the first place.
+	 */
+	if (!cpumask_equal(affinity, cpu_possible_mask))
+		action->flags |= IRQF_SHARED;
 
 	return action;
 }
