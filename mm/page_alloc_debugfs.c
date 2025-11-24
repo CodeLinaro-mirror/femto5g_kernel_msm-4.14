ce9b6c3f23560c7df3e2c32f784a38e7debef0fb
Juan Yescas <jyescas@google.com>
2025-11-24 10:54:42 -0800

    ANDROID: mm: Create dirs for nodes in the debugfs
    This change creates the root directory "mm" for the
    debug memory subsystem in debugfs. Inside the "mm"
    directory, it creates a dir for each node.
    
    The change was tested by mounting the debugfs fs and
    checking the content:
    
    mount -t debugfs none /sys/kernel/debug
    ls /sys/kernel/debug
    node-0
    node-1
    
    Bug: 456788322
    Change-Id: I40f70dbe4d88c6bf3144e880fe0542cffa3107d7
    Signed-off-by: Juan Yescas <jyescas@google.com>

--- mm/page_alloc_debugfs.c
+++ mm/page_alloc_debugfs.c
@@ -9,11 +9,31 @@
 
 #include <linux/debugfs.h>
 #include <linux/kernel.h>
+#include <linux/nodemask.h>
 #include <linux/printk.h>
+
+static inline void create_nodes_subdirs(struct dentry *mmdir)
+{
+	struct dentry *nodedir;
+	int nodeid;
+	char dirname[12];
+
+	for_each_online_node(nodeid) {
+		snprintf(dirname, sizeof(dirname), "node-%d", nodeid);
+		nodedir = debugfs_create_dir(dirname, mmdir);
+	}
+}
 
 static int __init page_alloc_debugfs_init(void)
 {
+	struct dentry *mmdir;
+
 	pr_info("Starting");
+	mmdir = debugfs_create_dir("mm", NULL);
+	if (IS_ERR(mmdir))
+		return PTR_ERR(mmdir);
+
+	create_nodes_subdirs(mmdir);
 
 	return 0;
 }
