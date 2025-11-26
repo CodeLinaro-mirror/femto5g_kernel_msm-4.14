13089cc305552d81f05c0de55e72fcf313fc36bf
T.J. Mercier <tjmercier@google.com>
2025-11-26 10:58:52 -0800

    ANDROID: ABI: refresh, CRC fixups and allowed breaks
    We need to add a new pointer to mm_struct, but there is only 1 ABI
    reservation. Use the mm_struct ABI reservation to point to a new
    mm_struct_abi_extend which contains a task_dma_buf_info pointer, and can
    be extended with other fields in the future.
    
    Since mm_struct_abi_extend is meant to be available for extension in the
    future, vendor modules may NOT access this pointer as the size of the
    struct cannot be expected to be stable.
    
    Bug: 459757379
    Bug: 461520624
    Change-Id: I1707f1184c7e97869c9e4eaff0bcdde13b3725df
    Signed-off-by: T.J. Mercier <tjmercier@google.com>
    (cherry picked from commit 5b6cecf56c7250013e532423509fd9aa77e53456)
    Signed-off-by: Lee Jones <joneslee@google.com>

--- drivers/dma-buf/dma-buf.c
+++ drivers/dma-buf/dma-buf.c
@@ -27,9 +27,9 @@
 #include <linux/mm.h>
 #include <linux/mount.h>
 #include <linux/pseudo_fs.h>
-#include <linux/fdtable.h>
 
 #ifndef __GENKSYMS__
+#include <linux/fdtable.h>
 #include <trace/events/kmem.h>
 #endif
 #include <trace/hooks/dmabuf.h>
@@ -531,7 +531,7 @@ int copy_dmabuf_info(u64 clone_flags, struct task_struct *task)
 
 		if (task->mm) {
 			refcount_inc(&task->dmabuf_info->refcnt);
-			task->mm->dmabuf_info = task->dmabuf_info;
+			task->mm->abi_extend->dmabuf_info = task->dmabuf_info;
 		}
 
 		if (task->files) {
@@ -553,7 +553,7 @@ int copy_dmabuf_info(u64 clone_flags, struct task_struct *task)
 
 	if (task->mm) {
 		refcount_inc(&child_dmabuf_info->refcnt);
-		task->mm->dmabuf_info = child_dmabuf_info;
+		task->mm->abi_extend->dmabuf_info = child_dmabuf_info;
 	}
 	if (task->files) {
 		refcount_inc(&child_dmabuf_info->refcnt);
@@ -667,7 +667,7 @@ static int dma_buf_mmap_internal(struct file *file, struct vm_area_struct *vma)
 
 	ret = dmabuf->ops->mmap(dmabuf, vma);
 	if (!ret) {
-		int err = dma_buf_account_task(dmabuf, vma->vm_mm->dmabuf_info);
+		int err = dma_buf_account_task(dmabuf, vma->vm_mm->abi_extend->dmabuf_info);
 
 		if (err)
 			pr_err("dmabuf accounting failed during mmap operation, err %d\n", err);
@@ -2029,7 +2029,7 @@ int dma_buf_mmap(struct dma_buf *dmabuf, struct vm_area_struct *vma,
 
 	ret = dmabuf->ops->mmap(dmabuf, vma);
 	if (!ret) {
-		int err = dma_buf_account_task(dmabuf, vma->vm_mm->dmabuf_info);
+		int err = dma_buf_account_task(dmabuf, vma->vm_mm->abi_extend->dmabuf_info);
 
 		if (err)
 			pr_err("dmabuf accounting failed during mmap operation, err %d\n", err);
