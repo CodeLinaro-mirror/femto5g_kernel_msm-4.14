bd1d0f85452832f0709764901c4a0bc1a0947270
Hu Xile <huxile@oppo.com>
2026-01-15 21:18:17 +0800

    ANDROID: vendor_hooks: optimize the runnable issue of critical tasks when switching in and out of the ext scheduler.
    Vendor hooks added:
    android_vh_enq_to_priq
    android_vh_scx_switch_repeat_skip
    
    To optimize the runnability and priority inversion issues of critical tasks when switching in and out of the Ext scheduler
    in scenario-based scheduling, we optimize in two ways:
    1. When switching in and out of the Ext scheduler, we perform a two-round switching. The first round switches critical tasks,
    and the second round switches ordinary tasks;
    2. When tasks are enqueued into the global DSQ during bypass, we organize them by PRIOQ instead of FIFO. This preserves priority
    differentiation even in bypass mode.
    Note: The Ext author does not recommend using the SCX_ENQ_DSQ_PRIQ flag for built-in queues. Therefore, we cannot enable priority-based
    enqueuing via upper-layer configuration of this flag. Instead, we implement trace_android_vh_scx_enq_to_priq within dispatch_enqueue.
    We have thoroughly assessed the risks of using priority enqueuing with built-in queues (as highlighted by the author) and confirm that
    our implementation effectively eliminates these risks.
    
    Bug: 476045817
    Bug: 478177253
    
    Change-Id: I219c64a906c1cfd08f87cd3a0c92681aed3da15b
    Signed-off-by: Hu Xile <huxile@oppo.com>
    (cherry picked from commit 71d96707d354edd904215608b3ee7e95f1353133)

--- kernel/sched/ext.c
+++ kernel/sched/ext.c
@@ -1674,6 +1674,8 @@ static void dispatch_enqueue(struct scx_dispatch_q *dsq, struct task_struct *p,
 			     u64 enq_flags)
 {
 	bool is_local = dsq->id == SCX_DSQ_LOCAL;
+	bool enq_priq = false;
+	struct rq *rq = task_rq(p);
 
 	WARN_ON_ONCE(p->scx.dsq || !list_empty(&p->scx.dsq_list.node));
 	WARN_ON_ONCE((p->scx.dsq_flags & SCX_TASK_DSQ_ON_PRIQ) ||
@@ -1703,7 +1705,8 @@ static void dispatch_enqueue(struct scx_dispatch_q *dsq, struct task_struct *p,
 		enq_flags &= ~SCX_ENQ_DSQ_PRIQ;
 	}
 
-	if (enq_flags & SCX_ENQ_DSQ_PRIQ) {
+	trace_android_vh_enq_to_priq(rq, dsq, p, &enq_priq);
+	if ((enq_flags & SCX_ENQ_DSQ_PRIQ) || enq_priq) {
 		struct rb_node *rbp;
 
 		/*
@@ -4617,6 +4620,7 @@ static void scx_ops_disable_workfn(struct kthread_work *work)
 	struct rhashtable_iter rht_iter;
 	struct scx_dispatch_q *dsq;
 	int i, kind;
+	int repeat = 0;
 
 	kind = atomic_read(&scx_exit_kind);
 	while (true) {
@@ -4677,12 +4681,18 @@ static void scx_ops_disable_workfn(struct kthread_work *work)
 
 	scx_ops_init_task_enabled = false;
 
+repeat_iter:
 	scx_task_iter_start(&sti);
 	while ((p = scx_task_iter_next_locked(&sti))) {
 		const struct sched_class *old_class = p->sched_class;
 		const struct sched_class *new_class =
 			__setscheduler_class(p->policy, p->prio);
 		struct sched_enq_and_set_ctx ctx;
+		bool skip = false;
+
+		trace_android_vh_scx_switch_repeat_skip(p, &skip, &repeat);
+		if (skip)
+			continue;
 
 		if (old_class != new_class && p->se.sched_delayed)
 			dequeue_task(task_rq(p), p, DEQUEUE_SLEEP | DEQUEUE_DELAYED);
@@ -4699,6 +4709,10 @@ static void scx_ops_disable_workfn(struct kthread_work *work)
 		scx_ops_exit_task(p);
 	}
 	scx_task_iter_stop(&sti);
+	if (repeat > 0) {
+		repeat++;
+		goto repeat_iter;
+	}
 	percpu_up_write(&scx_fork_rwsem);
 
 	/* no task is on scx, turn off all the switches and flush in-progress calls */
@@ -5166,6 +5180,7 @@ static int scx_ops_enable(struct sched_ext_ops *ops, struct bpf_link *link)
 	struct task_struct *p;
 	unsigned long timeout;
 	int i, cpu, node, ret;
+	int repeat = 0;
 
 	if (!cpumask_equal(housekeeping_cpumask(HK_TYPE_DOMAIN),
 			   cpu_possible_mask)) {
@@ -5398,16 +5413,22 @@ static int scx_ops_enable(struct sched_ext_ops *ops, struct bpf_link *link)
 	 * scx_tasks_lock.
 	 */
 	percpu_down_write(&scx_fork_rwsem);
+
+repeat_iter:
 	scx_task_iter_start(&sti);
 	while ((p = scx_task_iter_next_locked(&sti))) {
 		const struct sched_class *old_class = p->sched_class;
 		const struct sched_class *new_class =
 			__setscheduler_class(p->policy, p->prio);
 		struct sched_enq_and_set_ctx ctx;
+		bool skip = false;
 
 		if (!tryget_task_struct(p))
 			continue;
 
+		trace_android_vh_scx_switch_repeat_skip(p, &skip, &repeat);
+		if (skip)
+			continue;
 		if (old_class != new_class && p->se.sched_delayed)
 			dequeue_task(task_rq(p), p, DEQUEUE_SLEEP | DEQUEUE_DELAYED);
 
@@ -5424,6 +5445,10 @@ static int scx_ops_enable(struct sched_ext_ops *ops, struct bpf_link *link)
 		put_task_struct(p);
 	}
 	scx_task_iter_stop(&sti);
+	if (repeat > 0) {
+		repeat++;
+		goto repeat_iter;
+	}
 	percpu_up_write(&scx_fork_rwsem);
 
 	scx_ops_bypass(false);
