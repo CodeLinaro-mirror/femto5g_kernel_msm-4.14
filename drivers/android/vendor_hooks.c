624e5581f4ca8f06656f701518bf010c6229be47
Jianan Huang <huangjianan@xiaomi.com>
2026-01-19 10:27:21 +0800

    ANDROID: fs: add vendor hook to trace put_super
    This is to remove vendor specific per‑superblock data management
    structures when the filesystem is destroyed.
    
    Bug: 476798067
    Bug: 483833688
    Bug: 490243115
    Bug: 490941397
    Bug: 507357484
    
    Change-Id: I95011803a59cfa496c778aab6b885455449ac927
    Signed-off-by: Jianan Huang <huangjianan@xiaomi.com>
    (cherry picked from commit 9a0a18bc9fc223db65b98a0d1d8b9fa1d0d048ec)
    [Wenjie: Resolved minor conflict in fs/super.c.]
    Signed-off-by: Wenjie Qi <qiwenjie@xiaomi.com>
    (cherry picked from commit 4650088b3102abb9ef7e45b37a1b6075a580432a)
    Signed-off-by: Wenjie Qi <qiwenjie@xiaomi.com>
    (cherry picked from commit 9983e7a1b48a66db5fd98aaf196cca5b15c68f97)
    Signed-off-by: Wenjie Qi <qiwenjie@xiaomi.com>

--- drivers/android/vendor_hooks.c
+++ drivers/android/vendor_hooks.c
@@ -81,6 +81,7 @@
  * Export tracepoints that act as a bare tracehook (ie: have no trace event
  * associated with them) to allow external modules to probe them.
  */
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_put_super);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_sk_alloc);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_sk_free);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_refrigerator);
