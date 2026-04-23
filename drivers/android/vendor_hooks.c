ca81daadb2e5e8514d486f7a9b372aa0c03db000
Wenchao Hao <haowenchao@xiaomi.corp-partner.google.com>
2026-04-23 10:55:12 +0800

    ANDROID: vendor_hooks: Add 2 vendor hook in rmap.c
    Add 2 vendor hooks in rmap.c to trace folio's map count and perform
    some special policy.
    
    Bug: 505541412
    Bug: 537101514
    
    Change-Id: I1805b34a2d0aeba6e40bf1e29f3fe31ae4f79089
    Signed-off-by: Wenchao Hao <haowenchao@xiaomi.corp-partner.google.com>

--- drivers/android/vendor_hooks.c
+++ drivers/android/vendor_hooks.c
@@ -525,3 +525,5 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_fas_gpu_qos_update_tracer);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_nr_pcp_alloc);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_pcp_alloc_factor_adjust);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_read_swap_cache_async_timeout);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_add_file_rmap);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_remove_file_rmap);
--- include/trace/hooks/mm.h
+++ include/trace/hooks/mm.h
@@ -385,6 +385,12 @@ DECLARE_HOOK(android_vh_pcp_alloc_factor_adjust,
 DECLARE_RESTRICTED_HOOK(android_rvh_read_swap_cache_async_timeout,
 	TP_PROTO(size_t *count, bool *skip),
 	TP_ARGS(count, skip), 2);
+DECLARE_HOOK(android_vh_add_file_rmap,
+	TP_PROTO(struct page *page, bool compound),
+	TP_ARGS(page, compound));
+DECLARE_HOOK(android_vh_remove_file_rmap,
+	TP_PROTO(struct page *page, bool compound),
+	TP_ARGS(page, compound));
 #endif /* _TRACE_HOOK_MM_H */
 
 /* This part must be outside protection */
