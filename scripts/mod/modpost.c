BayLibre/android_kernel_common

ANDROID: modpost: add protected exports to vmlinux
Protected exports are placed in the __kexporttab section.

Bug: 458591624
Change-Id: Iab22863baefe7c4575604e18ed403aef8ee3b7c5
Signed-off-by: Siddharth Nayyar <sidnayyar@google.com>

scripts/mod/modpost.c
@@ -1908,6 +1908,21 @@ static void add_exported_symbols(struct buffer *buf, struct module *mod)
 	}
 }
 
+static void add_protected_exports(struct buffer *buf)
+{
+	struct module *mod;
+	struct symbol *sym;
+
+	buf_printf(buf, "\n");
+	list_for_each_entry(mod, &modules, list) {
+		if (mod->protect_exports) {
+			list_for_each_entry(sym, &mod->exported_symbols, list)
+				buf_printf(buf, "PROTECT_SYMBOL(%s);\n",
+					   sym->name);
+		}
+	}
+}
+
 /**
  * Record CRCs for unresolved symbols, supporting long names
  */
@@ -2094,6 +2109,7 @@ static void write_vmlinux_export_c_file(struct module *mod)
 		   "#include <linux/export-internal.h>\n");
 
 	add_exported_symbols(&buf, mod);
+	add_protected_exports(&buf);
 
 	buf_printf(&buf,
 		   "#include <linux/module.h>\n"

CodeLinaro-mirror/femto5g_kernel_msm-4.14

Bug: 519119852

scripts/mod/modpost.c
@@ -1917,6 +1917,21 @@ static void add_exported_symbols(struct buffer *buf, struct module *mod)
 	}
 }
 
+static void add_protected_exports(struct buffer *buf)
+{
+	struct module *mod;
+	struct symbol *sym;
+
+	buf_printf(buf, "\n");
+	list_for_each_entry(mod, &modules, list) {
+		if (mod->protect_exports) {
+			list_for_each_entry(sym, &mod->exported_symbols, list)
+				buf_printf(buf, "PROTECT_EXPORT(%s);\n",
+					   sym->name);
+		}
+	}
+}
+
 /**
  * Record CRCs for unresolved symbols, supporting long names
  */
@@ -2103,6 +2118,7 @@ static void write_vmlinux_export_c_file(struct module *mod)
 		   "#include <linux/export-internal.h>\n");
 
 	add_exported_symbols(&buf, mod);
+	add_protected_exports(&buf);
 
 	buf_printf(&buf,
 		   "#include <linux/module.h>\n"