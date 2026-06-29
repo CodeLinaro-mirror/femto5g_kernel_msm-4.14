c8ede8f15baf7234aa695f545f8486b632f81e29
Youjun Xu <xuyiguo@xiaomi.corp-partner.google.com>
2026-06-29 11:47:46 +0800

    ANDROID: pKVM: arm64: ffa_max_nr_constituents configurable
    When VPU/GPU media applications allocate secure buffers via FFA memory lend, the operation fails with -ENOMEM
    because pKVM's pre-allocated descriptor buffer (ffa_desc_buf) is too small to hold the FFA memory descriptor.
    
    The root cause is KVM_FFA_MAX_NR_CONSTITUENTS is set to 4096, which only supports up to 16MB of fully scattered (4KB-granularity) memory.
    So make it a configurable kernel argument
    
    Bug: 522651314
    Bug: 529663897
    Change-Id: Iba91f80b4649aa10db409b822fa38d0b1f88afad
    Signed-off-by: Youjun Xu <xuyiguo@xiaomi.corp-partner.google.com>
    (cherry picked from commit ef46e000366a02ca388c126fc638206f05a87451)
    Signed-off-by: Maheshwar Sakaray Lambade <msakaray@qualcomm.corp-partner.google.com>
    Signed-off-by: Manish Kumar <kumarma@qualcomm.corp-partner.google.com>

--- arch/arm64/include/asm/kvm_pkvm.h
+++ arch/arm64/include/asm/kvm_pkvm.h
@@ -438,10 +438,12 @@ static inline unsigned long pkvm_selftest_pages(void) { return 0; }
 extern u64 kvm_nvhe_sym(kvm_ffa_spm_nr_pages);
 
 /*
- * Maximum number of consitutents allowed in a descriptor. This number is
- * arbitrary, see comment below on SG_MAX_SEGMENTS in hyp_ffa_proxy_pages().
+ * Maximum number of constituents allowed in a descriptor. This number is
+ * arbitrary, and can be overridden via command line kvm-arm.ffa_max_nr_constituents.
+ * See comment below on SG_MAX_SEGMENTS in hyp_ffa_proxy_pages().
  */
 #define KVM_FFA_MAX_NR_CONSTITUENTS	4096
+extern size_t kvm_nvhe_sym(ffa_max_nr_constituents);
 
 enum pkvm_ffa_unmap_on_lend_mode {
 	PKVM_FFA_UNMAP_ON_LEND_OFF = 0,
@@ -469,7 +471,7 @@ static inline unsigned long hyp_ffa_proxy_pages(void)
 	 * it is sometimes abused, so let's allow larger descriptors and hope
 	 * for the best.
 	 */
-	BUILD_BUG_ON(KVM_FFA_MAX_NR_CONSTITUENTS < SG_MAX_SEGMENTS);
+	WARN_ON(kvm_nvhe_sym(ffa_max_nr_constituents) < SG_MAX_SEGMENTS);
 
 	/*
 	 * The hypervisor FFA proxy needs enough memory to buffer a fragmented
@@ -478,7 +480,7 @@ static inline unsigned long hyp_ffa_proxy_pages(void)
 	desc_max = sizeof(struct ffa_mem_region) +
 		   sizeof(struct ffa_mem_region_attributes) +
 		   sizeof(struct ffa_composite_mem_region) +
-		   KVM_FFA_MAX_NR_CONSTITUENTS * sizeof(struct ffa_mem_region_addr_range);
+		   kvm_nvhe_sym(ffa_max_nr_constituents) * sizeof(struct ffa_mem_region_addr_range);
 
 	/* Plus a page each for the hypervisor's RX and TX mailboxes. */
 	num_pages = (2 * KVM_FFA_MBOX_NR_PAGES) + DIV_ROUND_UP(desc_max, PAGE_SIZE);
