maltesermailo/android_kernel_pantah

ANDROID: vendor hooks: add hooks to support the functionality and management of multiple swap devices.
Add several hooks to the swap allocation and release processes to enable the selection of appropriate swap devices based on different memory states, thereby improving memory reclamation efficiency. Additionally, directly free the swap slots of newly added swap devices to prevent the unintended mixing of multiple swap slots.

Bug: 503420547
Change-Id: I9589164a216afc3f13ce41226bcfc5f8295120f3
Signed-off-by: chen hao <chen.hao@vivo.corp-partner.google.com>

drivers/android/vendor_hooks.c
@@ -747,3 +747,7 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_lock_task_exit);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_is_rndis_enabled);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_reasm_timer_adjust);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_read_swap_cache_async_timeout);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_check_swap_entry_range_free);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_free_swap_slot_bypass);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_folio_alloc_swap_bypass);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_pick_swap_device_bypass);

CodeLinaro-mirror/femto5g_kernel_msm-4.14

Add several hooks to the swap allocation and release processes to allow for the selection of appropriate swap devices based on different memory states, thereby improving memory reclamation efficiency. Additionally, newly added swap devices do not use percpu_swap_cluster to prevent the unorganized mixing of multiple swap slots.
Bug: 531878280
(cherry picked from commit 00e63fb9367ffa4eddbfc324bd8135dbd62bac48)
[hao:Resolved conflicts in mm/swapfile.c]
(cherry picked from commit 427935c0267e2bf81d7645b1962f3fa35e3200a6)

drivers/android/vendor_hooks.c
@@ -260,6 +260,7 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_check_mmap_file);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_check_nanosleep_syscall);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_check_new_page);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_check_set_ioprio);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_check_swap_entries_free);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_check_uninterruptible_tasks);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_check_uninterruptible_tasks_dn);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_clear_reclaimed_folio);
@@ -363,6 +364,7 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_flush_wq_wait_finish);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_flush_wq_wait_start);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_folio_add_file_rmap);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_folio_add_lru_folio_activate);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_folio_alloc_swap_finish);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_folio_end_writeback);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_folio_remove_rmap);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_folio_remove_rmap_ptes);
@@ -512,6 +514,7 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_percpu_rwsem_down_read);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_percpu_rwsem_init);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_percpu_rwsem_up_write);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_percpu_rwsem_wq_add);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_pick_swap_device_bypass);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_platform_pci_choose_state);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_platform_pci_get_power_state);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_platform_pci_power_manageable);
@@ -618,6 +621,7 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_smaps_pte_entry);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_sock_create);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_spec_promote_folio);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_split_large_folio_bypass);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_swap_alloc_fast_bypass);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_swap_writepage);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_swap_writepage_end);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_swap_writepage_start);
@@ -686,6 +690,7 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_ufs_update_sdev);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_ufs_update_sysfs);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_ufs_wait_for_uic_cmd);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_unreserve_highatomic_bypass);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_update_percpu_swap_cluster_bypass);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_update_uid_stats);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_uplink_send_msg);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_uprobes_uprobe_write);