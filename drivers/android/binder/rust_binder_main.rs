96f1f21b29368b67d4c59c32f87a20e2b965f76e
Jahnavi MN <jahnavimn@google.com>
2026-07-16 08:37:49 +0000

    BACKPORT: rust_binder: Implement BINDER_DEBUG_DEAD_TRANSACTION
    This adds dynamic debug logs for:
    - Releasing active transactions during thread stack unwinding.
    - Discarded transaction error codes when a thread exits.
    - Undelivered transaction acknowledgments (TRANSACTION_COMPLETE)
      upon thread exit.
    - Undelivered process death and freeze notifications when processes
      exit or die.
    - Undelivered transactions canceled due to target process death.
    
    We now store the process PID in `ThreadError`, `DeliverCode`, and
    `FreezeMessage` to ensure the correct PID is logged on cancellation.
    This is necessary because `cancel()` runs from background `kworkers`,
    which would otherwise print the wrong PID.
    
    Reviewed-by: Alice Ryhl <aliceryhl@google.com>
    Reviewed-by: Carlos Llamas <cmllamas@google.com>
    Signed-off-by: Jahnavi MN <jahnavimn@google.com>
    Link: https://patch.msgid.link/20260716-rust_binder_debug_mask-v4-7-3d7436c2d2f2@google.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    
    Bug: 520343899
    Change-Id: I2dfb0d71451d72b71639fb4bfe0e0ad0f6472c59
    (cherry picked from commit 5757ed4d9543ce1d5940c995e03fb0c6d32615cf)
    [aliceryhl: fix conflict due to AtomicU32 vs Atomic<u32>]
    Signed-off-by: Alice Ryhl <aliceryhl@google.com>
    (cherry picked from commit 883880854a72ca501f17af2062228a9aabbe929c)
    Signed-off-by: Alice Ryhl <aliceryhl@google.com>
    Bug: 562458064

--- drivers/android/binder/rust_binder_main.rs
+++ drivers/android/binder/rust_binder_main.rs
@@ -243,6 +243,7 @@ impl<T: ListArcSafe> DTRWrap<T> {
 struct DeliverCode {
     code: u32,
     skip: AtomicBool,
+    pid: i32,
 }
 
 kernel::list::impl_list_arc_safe! {
@@ -250,10 +251,11 @@ kernel::list::impl_list_arc_safe! {
 }
 
 impl DeliverCode {
-    fn new(code: u32) -> Self {
+    fn new(code: u32, pid: i32) -> Self {
         Self {
             code,
             skip: AtomicBool::new(false),
+            pid,
         }
     }
 
@@ -278,7 +280,15 @@ impl DeliverToRead for DeliverCode {
         Ok(true)
     }
 
-    fn cancel(self: DArc<Self>) {}
+    fn cancel(self: DArc<Self>) {
+        if !self.skip.load(Ordering::Relaxed) {
+            binder_debug!(
+                pid = self.pid,
+                DeadTransaction,
+                "undelivered TRANSACTION_COMPLETE"
+            );
+        }
+    }
 
     fn should_sync_wakeup(&self) -> bool {
         false
