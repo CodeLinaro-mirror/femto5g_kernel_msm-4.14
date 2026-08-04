9f54cbcaa0443ad95e2243e629851a56f0c1e2e2
liujinbao1 <liujinbao1@xiaomi.corp-partner.google.com>
2026-08-04 20:53:05 +0800

    ANDROID: add vendor hooks for erofs iostat monitoring
    Add vendor hooks to allow external modules to implement I/O latency
    monitoring.
    
    Bug: 542535040
    Bug: 548175899
    Change-Id: I7103c4b3be198e7e9c95f39075877e58fe24db43
    Signed-off-by: liujinbao1 <liujinbao1@xiaomi.corp-partner.google.com>
    (cherry picked from commit 69c756fc850f2f504b106c5affba729debf1971a)

--- fs/erofs/zdata.c
+++ fs/erofs/zdata.c
@@ -8,6 +8,7 @@
 #include <linux/psi.h>
 #include <linux/cpuhotplug.h>
 #include <trace/events/erofs.h>
+#include <trace/hooks/fs.h>
 
 #define Z_EROFS_PCLUSTER_MAX_PAGES	(Z_EROFS_PCLUSTER_MAX_SIZE / PAGE_SIZE)
 #define Z_EROFS_INLINE_BVECS		2
@@ -1644,6 +1645,8 @@ static void z_erofs_submissionqueue_endio(struct bio *bio)
 	struct bio_vec *bvec;
 	struct bvec_iter_all iter_all;
 
+	trace_android_vh_erofs_iostat_update(q->sb, bio);
+
 	bio_for_each_segment_all(bvec, bio, iter_all) {
 		struct page *page = bvec->bv_page;
 
@@ -1721,6 +1724,7 @@ static void z_erofs_submit_queue(struct z_erofs_decompress_frontend *f,
 			if (bio && (cur != last_pa ||
 				    last_bdev != mdev.m_bdev)) {
 submit_bio_retry:
+				trace_android_vh_erofs_iostat_submit(sb, bio);
 				submit_bio(bio);
 				if (memstall) {
 					psi_memstall_leave(&pflags);
@@ -1764,8 +1768,10 @@ submit_bio_retry:
 			move_to_bypass_jobqueue(pcl, qtail, owned_head);
 	} while (owned_head != Z_EROFS_PCLUSTER_TAIL);
 
-	if (bio)
+	if (bio) {
+		trace_android_vh_erofs_iostat_submit(sb, bio);
 		submit_bio(bio);
+	}
 	if (memstall)
 		psi_memstall_leave(&pflags);
 
