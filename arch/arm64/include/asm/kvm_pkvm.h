9d64e70af4dbd38624756b2975264dafc8c42b66
Vincent Donnefort <vdonnefort@google.com>
2026-08-21 10:58:10 +0000

    ANDROID: KVM: arm64: Replace kvm_ffa_unmap_on_lend static key with integer
    Unmap-on-lend configurations will require supporting multiple modes,
    making a binary static key inadequate. Since the unmap-on-lend checks
    are out of the hot path, replace the kvm_ffa_unmap_on_lend static key
    with the __pkvm_ffa_unmap_on_lend global integer instead.
    
    Bug: 536805079
    Change-Id: I7747b706143c637e1d64fc97a3e58fd32713e212
    Signed-off-by: Vincent Donnefort <vdonnefort@google.com>
    Signed-off-by: Bartłomiej Grzesik <bgrzesik@google.com>

--- arch/arm64/include/asm/kvm_pkvm.h
+++ arch/arm64/include/asm/kvm_pkvm.h
@@ -438,7 +438,11 @@ static inline unsigned long pkvm_selftest_pages(void) { return 0; }
  */
 #define KVM_FFA_MAX_NR_CONSTITUENTS	4096
 
-DECLARE_STATIC_KEY_FALSE(kvm_ffa_unmap_on_lend);
+extern int kvm_nvhe_sym(__pkvm_ffa_unmap_on_lend);
+static inline bool pkvm_ffa_unmap_on_lend(void)
+{
+	return kvm_nvhe_sym(__pkvm_ffa_unmap_on_lend);
+}
 
 static inline unsigned long hyp_ffa_proxy_pages(void)
 {
@@ -468,7 +472,7 @@ static inline unsigned long hyp_ffa_proxy_pages(void)
 	/* Plus a page each for the hypervisor's RX and TX mailboxes. */
 	num_pages = (2 * KVM_FFA_MBOX_NR_PAGES) + DIV_ROUND_UP(desc_max, PAGE_SIZE);
 
-	if (static_branch_unlikely(&kvm_ffa_unmap_on_lend))
+	if (pkvm_ffa_unmap_on_lend())
 		num_pages += KVM_FFA_SPM_HANDLE_NR_PAGES;
 
 	return num_pages;
