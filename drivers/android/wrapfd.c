c325e781786b10dbea3bb0171b1e5ea39cdb7e02
Isaac J. Manjarres <isaacmanjarres@google.com>
2026-09-16 22:07:50 +0000

    ANDROID: drivers/wrapfd: Prevent subtraction underflow in file load path
    When buf_offs + len are smaller than a page, the logic for reading
    directly into a dmabuf is incorrectly triggered, as the check for
    triggering that logic results in an underflow. Therefore, only take
    that code path if the subtraction wouldn't result in an underflow.
    
    Fixes: ebfc1c876d5f ("ANDROID: drivers/wrapfd: Support unaligned file/buffer offsets")
    Bug: 562601588
    Change-Id: Ie3ee21e0d31dfd30bb5d8a9435f5135de02e0b6a
    Signed-off-by: Isaac J. Manjarres <isaacmanjarres@google.com>
    (cherry picked from commit 1b1a96c608d8f0b105513268d0dcdd1f2198f6c7)

--- drivers/android/wrapfd.c
+++ drivers/android/wrapfd.c
@@ -357,6 +357,7 @@ static int wrap_io_prepare(struct file *file, loff_t file_offs, u8 *dst_buf, lof
 	size_t start_bounce_len = 0;
 	loff_t dio_buf_offs = buf_offs;
 	size_t dio_buf_len = 0;
+	loff_t dio_buf_limit;
 	size_t end_bounce_len;
 	size_t nr_bounce_pages;
 	size_t file_read_len = dio_aligned_len(file_offs, len);
@@ -391,7 +392,8 @@ static int wrap_io_prepare(struct file *file, loff_t file_offs, u8 *dst_buf, lof
 	 * the range we're reading into, and since direct I/O is done in units of pages,
 	 * ensure that there is at least a page to read.
 	 */
-	if (dio_buf_offs <= (buf_end - PAGE_SIZE))
+	if (!check_sub_overflow(buf_end, PAGE_SIZE, &dio_buf_limit) &&
+	    dio_buf_offs <= dio_buf_limit)
 		dio_buf_len = PAGE_ALIGN_DOWN(buf_end) - dio_buf_offs;
 
 	/*
