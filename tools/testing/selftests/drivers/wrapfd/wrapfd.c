CodeLinaro-mirror/femto5g_kernel_msm-4.14

commit 77b3c87e00dc34f55a08e146061370372f5bc272

ANDROID: drivers/wrapfd: Split I/O requests and submit asynchronously
When loading a file, the wrapfd driver issues a single request to the
VFS layer to read the portion of the file that the user requested. This
request is then divided up according to the maximum hardware transfer
size for the given storage medium, and processed sequentially, which
leads to higher load times.

Instead of issuing a single request, break up a given request into
approximately equal requests, and use the kernel's asynchronous I/O
functionality to queue those requests across several calls to the VFS
layer. This ultimately results in those requests being dispatched to
different queues in the storage medium concurrently. Since each request
is placed on a different queue, the storage medium can process it in
parallel, speeding up the overall I/O transfer.

The minimum sizes for each request, and the number of requests, can vary
depending on the hardware, so expose those as module parameters, so they
can be tuned as needed.

Also, modify the tests to retrieve the minimum bytes per request, so
that the tests can generate a file large enough that is guaranteed to
be split into multiple requests.

Note: while the I/O transfer is carried out asynchronously, control does
not return to userspace until the I/O transfer has terminated
completely. Therefore, this has no impact on current users.

Bug: 529365761
Change-Id: I9db90f3eb5c117466c44730947c3426c9f0f5c8d
Signed-off-by: Isaac J. Manjarres <isaacmanjarres@google.com>

drivers/android/wrapfd.c
@@ -79,14 +79,25 @@ struct wrap_io_ctx {
 	u8 *dst_buf;
 	size_t start_bounce_len;
 	size_t end_bounce_len;
+	struct file *file;
 	loff_t buf_offs;
 	struct data_segment file_seg;
 	/* The portion of the buffer that we read the file contents into without bouncing. */
 	struct data_segment dio_buf_seg;
+	long ret;
 	size_t bytes_read;
+	struct wrap_io_req *reqs;
+	unsigned int nr_reqs;
+	spinlock_t lock;
+	struct completion io_done;
+};
+
+struct wrap_io_req {
 	struct kiocb kiocb;
 	struct iov_iter iter;
 	struct kvec iov[MAX_NR_KVECS];
+	struct data_segment file_seg;
+	struct wrap_io_ctx *io_ctx;
 };
 
 struct dmabuf_load_param {
@@ -94,6 +105,30 @@ struct dmabuf_load_param {
 	struct iosys_map map;
 	struct wrap_io_ctx io_ctx;
 };
+
+static int nonzero_ulong_param_set(const char *val, const struct kernel_param *kp)
+{
+	unsigned long res;
+	int ret = kstrtoul(val, 0, &res);
+
+	if (ret)
+		return ret;
+	if (!res)
+		return -EINVAL;
+	*((unsigned long *)kp->arg) = res;
+	return 0;
+}
+
+static const struct kernel_param_ops nonzero_param_ops = {
+	.set = nonzero_ulong_param_set,
+	.get = param_get_ulong,
+};
+
+static unsigned long max_nr_load_reqs = 32;
+module_param_cb(max_nr_load_reqs, &nonzero_param_ops, &max_nr_load_reqs, 0644);
+
+static unsigned long min_bytes_per_req = SZ_1M;
+module_param_cb(min_bytes_per_req, &nonzero_param_ops, &min_bytes_per_req, 0644);
 
 static int dmabuf_content_create_wrap(struct wrap_content *content,
 				      struct wrap_ctx *ctx)
@@ -137,39 +172,166 @@ static size_t dio_aligned_len(loff_t file_offs, size_t len)
 	return PAGE_ALIGN(file_offs + len) - PAGE_ALIGN_DOWN(file_offs);
 }
 
-static void prepare_iocb_request(struct wrap_io_ctx *io_ctx, struct file *file)
+static void async_io_complete(struct kiocb *kiocb, long ret)
+{
+	struct wrap_io_req *req = container_of(kiocb, struct wrap_io_req, kiocb);
+	struct wrap_io_ctx *io_ctx = req->io_ctx;
+	unsigned long flags;
+
+	spin_lock_irqsave(&io_ctx->lock, flags);
+	if (ret < 0 && !io_ctx->ret)
+		io_ctx->ret = ret;
+	else
+		io_ctx->bytes_read += ret;
+
+	io_ctx->nr_reqs--;
+
+	if (!io_ctx->nr_reqs)
+		complete(&io_ctx->io_done);
+	spin_unlock_irqrestore(&io_ctx->lock, flags);
+}
+
+static void init_io_req(struct wrap_io_ctx *io_ctx, struct wrap_io_req *req, loff_t req_file_offs,
+			size_t req_len)
 {
 	unsigned int nr_segs = 0;
-	unsigned int cur_bounce_page = 0;
-	struct kiocb *kiocb = &io_ctx->kiocb;
-	int i;
+	loff_t global_file_offset;
+	loff_t global_dio_file_offset;
 
-	if (io_ctx->start_bounce_len) {
-		io_ctx->iov[nr_segs].iov_base = io_ctx->bounce_bufs[cur_bounce_page];
-		io_ctx->iov[nr_segs].iov_len = io_ctx->start_bounce_len;
+	/*
+	 * These are global in the context of the overall I/O request. The first represents the
+	 * offset into the file at which we start reading from for the overall request. The second
+	 * is the offset at which we read from the file directly into the destination buffer.
+	 */
+	global_file_offset = PAGE_ALIGN_DOWN(io_ctx->file_seg.offs);
+	global_dio_file_offset = global_file_offset + io_ctx->start_bounce_len;
+
+	init_sync_kiocb(&req->kiocb, io_ctx->file);
+	req->kiocb.ki_pos = req_file_offs;
+	req->kiocb.ki_flags |= IOCB_DIRECT;
+	req->kiocb.ki_complete = async_io_complete;
+	req->file_seg.offs = req_file_offs;
+	req->file_seg.len = req_len;
+	req->io_ctx = io_ctx;
+
+	/*
+	 * If there's a start bounce buffer, it's always for the first page in the overall read
+	 * request.
+	 */
+	if (io_ctx->start_bounce_len && (req_file_offs == global_file_offset)) {
+		req->iov[nr_segs].iov_base = io_ctx->bounce_bufs[0];
+		req->iov[nr_segs].iov_len = io_ctx->start_bounce_len;
+		req_len -= io_ctx->start_bounce_len;
+		req_file_offs += io_ctx->start_bounce_len;
 		nr_segs++;
-		cur_bounce_page++;
 	}
 
-	if (io_ctx->dio_buf_seg.len) {
-		io_ctx->iov[nr_segs].iov_base = io_ctx->dst_buf + io_ctx->dio_buf_seg.offs;
-		io_ctx->iov[nr_segs].iov_len = io_ctx->dio_buf_seg.len;
+	/*
+	 * Handle the case where the sub-request pertains to data copied directly from the file to
+	 * the destination buffer.
+	 */
+	if (req_len && io_ctx->dio_buf_seg.len && global_dio_file_offset <= req_file_offs) {
+		size_t dio_len;
+
+		/*
+		 * The offset into the buffer for this request should be the base of where we start
+		 * reading directly into it, plus how much we've already read into the region in
+		 * previous requests.
+		 */
+		req->iov[nr_segs].iov_base = io_ctx->dst_buf + io_ctx->dio_buf_seg.offs +
+					     (req_file_offs - global_dio_file_offset);
+		/*
+		 * The amount of data to read is the smaller of the two terms:
+		 *
+		 * 1. How much data is left for this request.
+		 * 2. How much data there is left in the file region that gets loaded directly
+		 * into the buffer, which is taken as the difference between the current position
+		 * in the file and the end of that region.
+		 */
+		dio_len = min_t(size_t, req_len, global_dio_file_offset + io_ctx->dio_buf_seg.len -
+				req_file_offs);
+		req->iov[nr_segs].iov_len = dio_len;
+		req_len -= dio_len;
+		req_file_offs += dio_len;
 		nr_segs++;
 	}
 
-	for (i = 0; i < io_ctx->end_bounce_len / PAGE_SIZE; i++) {
-		io_ctx->iov[nr_segs].iov_base = io_ctx->bounce_bufs[cur_bounce_page];
-		io_ctx->iov[nr_segs].iov_len = PAGE_SIZE;
-		nr_segs++;
-		cur_bounce_page++;
+	if (req_len && io_ctx->end_bounce_len) {
+		/*
+		 * The offset into the file that is copied into the end bounce buffer(s) for the
+		 * overall request.
+		 */
+		loff_t global_end_bounce_file_offs = global_file_offset +
+						dio_aligned_len(io_ctx->file_seg.offs,
+								io_ctx->file_seg.len) -
+						io_ctx->end_bounce_len;
+		int start_bounce_idx;
+
+		/*
+		 * The last two pages in the overall read request can be bounce pages, so we
+		 * calculate which pages to use here. If there's a bounce page at the beginning,
+		 * then start at index 1.
+		 *
+		 * Since we know the file offset that corresponds to the start of the file data
+		 * that will be copied into the end pages and the length, we use that and our
+		 * current position to track which one of the end pages to use.
+		 */
+		start_bounce_idx = (io_ctx->start_bounce_len ? 1 : 0) +
+				   ((req_file_offs - global_end_bounce_file_offs) / PAGE_SIZE);
+
+		WARN_ON(req_len > PAGE_SIZE * 2);
+
+		while (req_len) {
+			req->iov[nr_segs].iov_base = io_ctx->bounce_bufs[start_bounce_idx];
+			req->iov[nr_segs].iov_len = PAGE_SIZE;
+			req_len -= PAGE_SIZE;
+			nr_segs++;
+		}
 	}
 
-	init_sync_kiocb(kiocb, file);
-	/* File offset and size must be page aligned for direct I/O requests. */
-	kiocb->ki_pos = PAGE_ALIGN_DOWN(io_ctx->file_seg.offs);
-	kiocb->ki_flags |= IOCB_DIRECT;
-	iov_iter_kvec(&io_ctx->iter, ITER_DEST, io_ctx->iov, nr_segs,
-		      dio_aligned_len(io_ctx->file_seg.offs, io_ctx->file_seg.len));
+	WARN_ON(req_len);
+
+	iov_iter_kvec(&req->iter, ITER_DEST, req->iov, nr_segs, req->file_seg.len);
+}
+
+static int prepare_read_reqs(struct wrap_io_ctx *io_ctx)
+{
+	struct wrap_io_req *reqs;
+	unsigned int nr_reqs, i;
+	loff_t cur_file_offs;
+	size_t remaining_len = dio_aligned_len(io_ctx->file_seg.offs, io_ctx->file_seg.len);
+	size_t len_per_req;
+
+	/*
+	 * Try to split the I/O request evenly into MAX_NR_LOAD_REQS pieces, as long as each piece
+	 * is at least MIN_BYTES_PER_REQ in size, but no larger than MAX_RW_COUNT, since the VFS
+	 * layer cannot handle anything larger than that in one invocation.
+	 *
+	 * PAGE_ALIGN the length to ensure direct I/O requirements are upheld.
+	 */
+	len_per_req = PAGE_ALIGN(clamp_t(size_t, remaining_len / max_nr_load_reqs,
+					 min_bytes_per_req, MAX_RW_COUNT));
+
+	nr_reqs = DIV_ROUND_UP(remaining_len, len_per_req);
+
+	/* nr_reqs could be large, so use kvcalloc() just in case. */
+	reqs = kvzalloc_objs(*reqs, nr_reqs);
+	if (!reqs)
+		return -ENOMEM;
+
+	cur_file_offs = PAGE_ALIGN_DOWN(io_ctx->file_seg.offs);
+
+	for (i = 0; i < nr_reqs; i++) {
+		size_t req_len = min(len_per_req, remaining_len);
+
+		init_io_req(io_ctx, &reqs[i], cur_file_offs, req_len);
+		cur_file_offs += req_len;
+		remaining_len -= req_len;
+	}
+
+	io_ctx->reqs = reqs;
+	io_ctx->nr_reqs = nr_reqs;
+	return 0;
 }
 
 /*
@@ -253,12 +415,19 @@ static int wrap_io_prepare(struct file *file, loff_t file_offs, u8 *dst_buf, lof
 	io_ctx->dst_buf = dst_buf;
 	io_ctx->start_bounce_len = start_bounce_len;
 	io_ctx->end_bounce_len = end_bounce_len;
+	io_ctx->file = file;
 	io_ctx->buf_offs = buf_offs;
 	io_ctx->file_seg.offs = file_offs;
 	io_ctx->file_seg.len = len;
 	io_ctx->dio_buf_seg.offs = dio_buf_offs;
 	io_ctx->dio_buf_seg.len = dio_buf_len;
-	prepare_iocb_request(io_ctx, file);
+	spin_lock_init(&io_ctx->lock);
+	init_completion(&io_ctx->io_done);
+
+	ret = prepare_read_reqs(io_ctx);
+	if (ret)
+		goto err_free_bounce_pages;
+
 	return 0;
 
 err_free_bounce_pages:
@@ -269,25 +438,44 @@ err_free_bounce_pages:
 
 }
 
-static ssize_t wrap_io_read(struct wrap_io_ctx *io_ctx, struct file *file)
+static int wrap_io_read(struct wrap_io_ctx *io_ctx)
 {
-	ssize_t bytes_read;
-	loff_t file_offs = io_ctx->file_seg.offs;
-	size_t dio_read_len = dio_aligned_len(file_offs, io_ctx->file_seg.len);
+	ssize_t ret;
+	unsigned int i, nr_reqs;
 
-	while (io_ctx->bytes_read < dio_read_len) {
-		io_ctx->iter.count = min_t(size_t, MAX_RW_COUNT,
-					   PAGE_ALIGN(dio_read_len - io_ctx->bytes_read));
+	/*
+	 * io_ctx->nr_reqs is manipulated from async_io_complete(), to determine when the I/O is
+	 * complete, so cache it before it can be manipulated. No lock is required here since we
+	 * haven't started reading anything yet.
+	 */
+	nr_reqs = io_ctx->nr_reqs;
 
-		bytes_read = vfs_iocb_iter_read(file, &io_ctx->kiocb, &io_ctx->iter);
-		if (bytes_read <= 0)
-			return bytes_read;
+	for (i = 0; i < nr_reqs; i++) {
+		struct wrap_io_req *req = &io_ctx->reqs[i];
 
-		io_ctx->bytes_read += bytes_read;
+		/*
+		 * ret == -EIOCBQUEUED => I/O request was queued successfully.
+		 * ret >= 0 => I/O request was satisfied synchronously.
+		 * ret < 0 => error.
+		 *
+		 * If an error is encountered, record the first one. We have to call
+		 * async_io_complete() if the request is not being processed asynchronously to
+		 * ensure that the nr_reqs context field is decremented properly so that we don't
+		 * block indefinitely in the wait_for_completion() call later.
+		 */
+		ret = vfs_iocb_iter_read(io_ctx->file, &req->kiocb, &req->iter);
+		if (ret != -EIOCBQUEUED)
+			async_io_complete(&req->kiocb, ret);
 	}
 
+	wait_for_completion(&io_ctx->io_done);
+
+	if (io_ctx->ret)
+		return io_ctx->ret;
+
 	/* File was too short / early EOF. */
-	return io_ctx->bytes_read < offset_in_page(file_offs) + io_ctx->file_seg.len ? -EINVAL : 0;
+	return io_ctx->bytes_read < offset_in_page(io_ctx->file_seg.offs) + io_ctx->file_seg.len ?
+	       -EINVAL : 0;
 }
 
 static void wrap_io_complete(struct wrap_io_ctx *io_ctx)
@@ -341,6 +529,7 @@ static void wrap_io_complete(struct wrap_io_ctx *io_ctx)
 	WARN_ON(tot_len);
 
 out:
+	kvfree(io_ctx->reqs);
 	for (i = 0; i < ((io_ctx->start_bounce_len + io_ctx->end_bounce_len) / PAGE_SIZE); i++)
 		free_page((unsigned long)io_ctx->bounce_bufs[i]);
 
@@ -409,7 +598,7 @@ static int dmabuf_content_load(struct wrap_content *content, struct file *file,
 	if (ret < 0)
 		return ret;
 
-	ret = wrap_io_read(&param.io_ctx, file);
+	ret = wrap_io_read(&param.io_ctx);
 	dmabuf_content_load_complete(&param);
 	return ret;
 }

tools/testing/selftests/drivers/wrapfd/wrapfd.c
@@ -162,7 +162,30 @@ FIXTURE(wrapfd_tests)
 	int fd;
 };
 
-#define FILE_SZ_PAGES	100
+static size_t get_file_size(size_t page_size)
+{
+	size_t min_req_size = 100 * page_size - page_size / 2;
+	FILE *min_req_size_fp;
+
+	min_req_size_fp = fopen("/sys/module/wrapfd/parameters/min_bytes_per_req", "r");
+	if (!min_req_size_fp)
+		return min_req_size;
+
+	if (fscanf(min_req_size_fp, "%zu\n", &min_req_size) != 1)
+		goto out;
+
+	/*
+	 * Intentionally make the file size large enough so that the driver divides loads into
+	 * multiple requests, and make the file size page unaligned if necessary.
+	 */
+	min_req_size *= 3;
+	if (IS_ALIGNED(min_req_size, page_size))
+		min_req_size -= page_size / 2;
+
+out:
+	fclose(min_req_size_fp);
+	return min_req_size;
+}
 
 FIXTURE_SETUP(wrapfd_tests)
 {
@@ -176,8 +199,7 @@ FIXTURE_SETUP(wrapfd_tests)
 		SKIP(return, "Skipping all tests; kernel lacks wrapfd support");
 
 	self->page_size = (size_t)sysconf(_SC_PAGESIZE);
-	/* Intentionally make the file size page unaligned */
-	self->size = self->page_size * FILE_SZ_PAGES - self->page_size / 2;
+	self->size = get_file_size(self->page_size);
 
 	self->dev_fd = open("/dev/wrapfd", O_RDONLY);
 	ASSERT_TRUE(self->dev_fd >= 0);
@@ -444,6 +466,15 @@ static int __test_loads(struct __test_metadata *_metadata,
 
 	ret = load_and_cmp(_metadata, self, wrapfd, self->page_size, self->page_size,
 			   self->size - self->page_size);
+	EXPECT_EQ(ret, 0)
+		return ret;
+
+	/*
+	 * Test logic to use a bounce buffer at the start and end with a large enough request
+	 * that gets split into multiple sub-requests.
+	 */
+	ret = load_and_cmp(_metadata, self, wrapfd, self->page_size / 2, self->page_size / 2,
+			   self->size - (self->page_size / 2));
 	EXPECT_EQ(ret, 0)
 		return ret;
 

commit c7aa2e5fd4afbaf4584bb9fb67c11bd9278464f3

ANDROID: drivers/wrapfd: Fix end bounce buffer overwrites
When using two bounce buffers for the end of the file region
of interest, we need to increment start_bounce_idx, so that we
tell the VFS layer to load the content of the two file pages into two
distinct buffers. That is not happening at the moment, and causes
the content from both file pages to be written to a single buffer,
so increment the counter as originally intended.

Add a test as well, to ensure this doesn't regress again.

Fixes: 77b3c87e00dc ("ANDROID: drivers/wrapfd: Split I/O requests and submit asynchronously")
Bug: 562601588
Change-Id: I5aec7f3106e9555bab068eaa076abf091f4b5064
Signed-off-by: Isaac J. Manjarres <isaacmanjarres@google.com>

drivers/android/wrapfd.c
@@ -286,6 +286,7 @@ static void init_io_req(struct wrap_io_ctx *io_ctx, struct wrap_io_req *req, lof
 			req->iov[nr_segs].iov_len = PAGE_SIZE;
 			req_len -= PAGE_SIZE;
 			nr_segs++;
+			start_bounce_idx++;
 		}
 	}
 

tools/testing/selftests/drivers/wrapfd/wrapfd.c
@@ -478,6 +478,16 @@ static int __test_loads(struct __test_metadata *_metadata,
 	EXPECT_EQ(ret, 0)
 		return ret;
 
+	/*
+	 * Test logic to use a bounce buffer at the start and end with 2 bounce buffers at the end.
+	 *
+	 * 2 bounce buffers get allocated because after accounting for the start buffer, the
+	 * remaining length to be copied at the proper offset crosses a page boundary.
+	 */
+	ret = load_and_cmp(_metadata, self, wrapfd, self->page_size - 1, 0, self->page_size + 4);
+	EXPECT_EQ(ret, 0)
+		return ret;
+
 	/* Save this one for last since subsequent tests run comparisons on the entire buffer. */
 	ret = load_and_cmp(_metadata, self, wrapfd, 0, 0, self->size);
 	EXPECT_EQ(ret, 0);